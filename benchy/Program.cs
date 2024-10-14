67fce73e0268ddd19a1cfb787c78524e82b67860
Barion <ametrin.studios@gmail.com>
2024-10-14 22:27:53 +0200

    yea

--- benchy/Program.cs
+++ benchy/Program.cs
@@ -2,14 +2,18 @@
 using Ametrin.Optional.Benchy;
 using BenchmarkDotNet.Running;
 
+Option<int> lolo1 = 1;
+Result<int> lolr1 = 1;
+Result<int, Exception> lole1 = 1;
 
 Option<int> lolo = Option.None<int>();
 Result<int> lolr = Result.Fail<int>("Error");
 Result<int, Exception> lole = Result.Fail<int, Exception>(new Exception());
 
 Option<int> lolo2 = default;
-Result<int> lolrd = default;
-Result<int> lolr2 = "";
+Result<int> lolr2 = "Error";
 Result<int, Exception> lole2 = new Exception();
+
+Result<int> lolrd = default;
 
 BenchmarkRunner.Run<Benchmarks>();
