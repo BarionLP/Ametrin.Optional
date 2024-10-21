1071058eae505271a24c66cb89b0a88e524f1c74
Barion <ametrin.studios@gmail.com>
2024-10-21 12:03:44 +0200

    updated ErrorState to be the equivalent of ErrorState<Exception>

--- src/ErrorState.cs
+++ src/ErrorState.cs
@@ -1,26 +1,27 @@
 namespace Ametrin.Optional;
 
-[Obsolete("Use Option")]
 public readonly struct ErrorState : IEquatable<ErrorState>
 {
-    internal readonly bool _success;
-    public ErrorState() : this(true) { }
-    internal ErrorState(bool success)
+    internal readonly bool _isFail;
+    internal readonly Exception _error;
+    public ErrorState() : this(false, default!) { }
+    internal ErrorState(bool isFail, Exception error)
     {
-        _success = success;
+        _isFail = isFail;
+        _error = error;
     }
 
-    public static ErrorState Success() => new(true);
-    public static ErrorState Fail() => new(false);
+    public static ErrorState Success() => new();
+    public static ErrorState Fail(Exception? error = null) => new(true, error ?? new Exception());
 
-    public static ErrorState<T> Success<T>() => new(default!, true);
-    public static ErrorState<T> Fail<T>(T error) => new(error, false);
+    public static ErrorState<T> Success<T>() => new();
+    public static ErrorState<T> Fail<T>(T error) => new(true, error);
 
-    public TResult Select<TResult>(Func<TResult> success, Func<TResult> fail) => _success ? success() : fail();
+    public TResult Select<TResult>(Func<TResult> success, Func<TResult> fail) => _isFail ? fail() : success();
 
     public void IfSuccess(Action action)
     {
-        if (_success)
+        if (!_isFail)
         {
             action();
         }
@@ -28,38 +29,42 @@ public readonly struct ErrorState : IEquatable<ErrorState>
 
     public void IfFail(Action action)
     {
-        if (!_success)
+        if (_isFail)
         {
             action();
         }
     }
 
-    public override string ToString() => _success ? "Success" : "None";
-    public override int GetHashCode() => _success.GetHashCode();
+    public override string ToString() => _isFail ? _error?.ToString() ?? "Fail" : "Success";
+    public override int GetHashCode() => _isFail ? HashCode.Combine(_isFail.GetHashCode(), _error.GetHashCode()) : _isFail.GetHashCode();
     public override bool Equals(object? obj) => obj is ErrorState s && Equals(s);
-    public bool Equals(ErrorState other) => _success == other._success;
+    public bool Equals(ErrorState other)
+        => _isFail ? other._isFail && _error == other._error : !other._isFail;
+
     public static bool operator ==(ErrorState left, ErrorState right) => left.Equals(right);
     public static bool operator !=(ErrorState left, ErrorState right) => !(left == right);
 
-    public static implicit operator bool(ErrorState state) => state._success;
-    public static implicit operator ErrorState(bool state) => new(state);
+    public static implicit operator bool(ErrorState state) => !state._isFail;
+    public static implicit operator ErrorState(Exception? error) => error is null ? Success() : Fail(error);
 }
 
 public readonly struct ErrorState<T> : IEquatable<ErrorState<T>>
 {
-    internal readonly bool _success;
+    public bool IsSuccess => !_isFail;
+    public bool IsFail => _isFail;
+    internal readonly bool _isFail;
     internal readonly T _error;
-    public ErrorState() : this(default!, true) { }
-    internal ErrorState(T error, bool success)
+    public ErrorState() : this(false, default!) { }
+    internal ErrorState(bool isFail, T error)
     {
-        _success = success;
+        _isFail = isFail;
         _error = error;
     }
 
-    public TResult Select<TResult>(Func<TResult> success, Func<T, TResult> fail) => _success ? success() : fail(_error);
+    public TResult Select<TResult>(Func<TResult> success, Func<T, TResult> fail) => _isFail ? fail(_error) : success();
     public void IfSuccess(Action action)
     {
-        if (_success)
+        if (!_isFail)
         {
             action();
         }
@@ -67,35 +72,33 @@ public readonly struct ErrorState<T> : IEquatable<ErrorState<T>>
 
     public void IfFail(Action<T> action)
     {
-        if (!_success)
+        if (_isFail)
         {
             action(_error);
         }
     }
-    
+
     public void Consume(Action success, Action<T> fail)
     {
-        if (_success)
-        {
-            success();
-        }
-        else
+        if (_isFail)
         {
             fail(_error);
         }
+        else
+        {
+            success();
+        }
     }
 
-    public override string ToString() => _success ? "Success" : _error?.ToString() ?? "Fail";
-    public override int GetHashCode() => _success ? _success.GetHashCode() : HashCode.Combine(_success.GetHashCode(), _error!.GetHashCode());
+    public override string ToString() => _isFail ? _error?.ToString() ?? "Fail" : "Success";
+    public override int GetHashCode() => _isFail ? HashCode.Combine(_isFail.GetHashCode(), _error!.GetHashCode()) : _isFail.GetHashCode();
     public override bool Equals(object? obj) => obj is ErrorState<T> s && Equals(s);
-    public bool Equals(ErrorState<T> other) => _success == other._success && (_success || EqualityComparer<T>.Default.Equals(_error, other._error));
-
-    public bool IsSuccess() => _success;
-    public bool IsFail() => !_success;
+    public bool Equals(ErrorState<T> other)
+        => _isFail ? other._isFail && EqualityComparer<T>.Default.Equals(_error, other._error) : !other._isFail;
 
     public static bool operator ==(ErrorState<T> left, ErrorState<T> right) => left.Equals(right);
     public static bool operator !=(ErrorState<T> left, ErrorState<T> right) => !(left == right);
 
-    public static implicit operator bool(ErrorState<T> state) => state._success;
+    public static implicit operator bool(ErrorState<T> state) => !state._isFail;
     public static implicit operator ErrorState<T>(T? state) => state is T t ? ErrorState.Fail(t) : ErrorState.Success<T>();
 }
