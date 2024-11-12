b467c281edc48d2534b58000b9d7b3d5fdc5f367
Barion <ametrin.studios@gmail.com>
2024-11-12 17:04:16 +0100

    fix

--- src/Extensions/FileSystemInfoExtensions.cs
+++ src/Extensions/FileSystemInfoExtensions.cs
@@ -1,6 +1,6 @@
 using System.IO;
 
-namespace Ametrin.Optional.Extensions;
+namespace Ametrin.Optional;
 
 public static class FileSystemInfoExtensions
 {
@@ -12,5 +12,4 @@ public static class FileSystemInfoExtensions
 
     public static Result<T> WhereExists<T>(this Result<T> result) where T : FileSystemInfo
         => result.Where(static info => info.Exists, static info => new FileNotFoundException(null, info.FullName));
-
 }
