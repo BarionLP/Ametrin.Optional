81b25464ba6348200fd3e7ebd29391cce004470b
Barion <ametrin.studios@gmail.com>
2025-01-09 16:40:15 +0100

    added some SelectAsync

--- benchy/Program.cs
+++ benchy/Program.cs
@@ -1,9 +1,15 @@
 ﻿using Ametrin.Optional;
 using Ametrin.Optional.Benchy;
+using Ametrin.Optional.Benchy.Examples;
 using BenchmarkDotNet.Running;
 
 var option = RefOption.Success(Span<byte>.Empty);
 var other = option.WhereNot(span => span.IsEmpty).Select(span => Convert.ToHexString(span));
 option.Or([]);
 
-BenchmarkRunner.Run<Benchmarks>();
+// var lol = Option.Success(new FileInfo("lol.txt"));
+// var content = await lol.SelectAsync(async file => await File.ReadAllTextAsync(file.FullName));
+
+// Console.WriteLine(content);
+
+BenchmarkRunner.Run<TestBenchmarks>();
