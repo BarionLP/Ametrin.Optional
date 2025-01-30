1cbefc803ab365aeffe98fb441e96b61db64c76c
Barion <ametrin.studios@gmail.com>
2025-01-30 14:17:56 +0100

    rename select to map

---
+++ src/Operations/TryMap.cs
@@ -0,0 +1,129 @@
+namespace Ametrin.Optional;
+
+partial struct Option<TValue>
+{
+    public Option<TResult> TryMap<TResult>(Func<TValue, TResult> map)
+    {
+        if (_hasValue)
+        {
+            try
+            {
+                return map(_value);
+            }
+            catch { }
+        }
+
+        return default;
+    }
+}
+
+partial struct Result<TValue>
+{
+    public Result<TResult> TryMap<TResult>(Func<TValue, TResult> map)
+    {
+        if (!_hasValue)
+        {
+            return _error;
+        }
+
+        try
+        {
+            return map(_value);
+        }
+        catch (Exception e)
+        {
+            return e;
+        }
+    }
+
+    public Result<TResult, TNewError> TryMap<TResult, TNewError>(Func<TValue, TResult> map, Func<Exception, TNewError> errormap)
+    {
+        if (!_hasValue)
+        {
+            return errormap(_error);
+        }
+
+        try
+        {
+            return map(_value);
+        }
+        catch (Exception e)
+        {
+            return errormap(e);
+        }
+    }
+}
+
+partial struct Result<TValue, TError>
+{
+    public Result<TResult, TError> TryMap<TResult>(Func<TValue, TResult> map, Func<Exception, TError> errormap)
+    {
+        if (!_hasValue)
+        {
+            return _error;
+        }
+
+        try
+        {
+            return map(_value);
+        }
+        catch (Exception e)
+        {
+            return errormap(e);
+        }
+    }
+}
+
+partial struct RefOption<TValue>
+{
+    public RefOption<TResult> TryMap<TResult>(Func<TValue, TResult> map)
+        where TResult : struct, allows ref struct
+    {
+        if (!_hasValue)
+        {
+            return default;
+        }
+
+        try
+        {
+            return map(_value);
+        }
+        catch
+        {
+            return default;
+        }
+    }
+}
+
+public static class TryMapOptionExtensions
+{
+    public static Option<TResult> TryMap<TValue, TResult>(this RefOption<TValue> option, Func<TValue, TResult> map)
+        where TValue : struct, allows ref struct
+        where TResult : class
+    {
+        if (option._hasValue)
+        {
+            try
+            {
+                return map(option._value);
+            }
+            catch { }
+        }
+
+        return default;
+    }
+
+    public static RefOption<TResult> TryMap<TValue, TResult>(this Option<TValue> option, Func<TValue, TResult> map)
+        where TResult : struct, allows ref struct
+    {
+        if (option._hasValue)
+        {
+            try
+            {
+                return RefOption.Success(map(option._value));
+            }
+            catch { }
+        }
+        return default;
+    }
+}
---
+++ test/MapTests.cs
@@ -0,0 +1,50 @@
+namespace Ametrin.Optional.Test;
+
+public sealed class MapTests
+{
+    [Test]
+    public async Task Map_Success_Test()
+    {
+        await Assert.That(Option.Success(1).Map(i => i + 1)).IsSuccess(2);
+        await Assert.That(Option.Success(1).Map(i => Option.Success(i))).IsSuccess(1);
+        await Assert.That(Result.Success(1).Map(i => i + 1)).IsSuccess(2);
+        await Assert.That(Result.Success(1).Map(i => i + 1, e => e.Message)).IsSuccess(2);
+        await Assert.That(Result.Success(1).Map(i => Result.Success(i))).IsSuccess(1);
+        await Assert.That(Result.Success<int, string>(1).Map(i => i + 1)).IsSuccess(2);
+        await Assert.That(Result.Success<int, int>(1).Map(i => i + 1, error => error.ToString())).IsSuccess(2);
+        await Assert.That(Result.Success<int, string>(5).Map(i => i + 1, error => new Exception(error))).IsSuccess();
+        await Assert.That(Result.Success<int, string>(1).Map(i => Result.Success<int, string>(i))).IsSuccess(1);
+        await Assert.That(RefOption.Success<Span<char>>([]).Map(s => new string(s))).IsSuccess("");
+        await Assert.That(OptionsMarshall.IsSuccess(RefOption.Success<Span<char>>([]).Map(s => s))).IsTrue();
+        await Assert.That(OptionsMarshall.IsSuccess(Option.Success("").MapAsSpan())).IsTrue();
+
+        await Assert.That(Option.Success().Map(() => "yay", () => "nay")).IsEqualTo("yay");
+        await Assert.That(ErrorState.Success().Map(() => "yay", e => "nay")).IsEqualTo("yay");
+        await Assert.That(ErrorState.Success<int>().Map(() => "yay", e => "nay")).IsEqualTo("yay");
+    }
+
+    [Test]
+    public async Task Map_Error_Test()
+    {
+        await Assert.That(Option.Error<int>().Map(i => i + 1)).IsError();
+        await Assert.That(Option.Error<int>().Map(i => Option.Success(i))).IsError();
+        await Assert.That(Option.Success(1).Map(i => Option.Error<int>())).IsError();
+        await Assert.That(Result.Error<int>().Map(i => i + 1)).IsError();
+        await Assert.That(Result.Error<int>().Map(i => i + 1, e => e.Message)).IsError();
+        await Assert.That(Result.Error<int>(new FormatException()).Map(i => Result.Success(i))).IsErrorOfType<int, FormatException>();
+        await Assert.That(Result.Success(1).Map(i => Result.Error<int>(new ArgumentException()))).IsErrorOfType<int, ArgumentException>();
+        await Assert.That(Result.Error<int, string>("").Map(i => i + 1)).IsError("");
+        await Assert.That(Result.Error<int, int>(5).Map(i => i + 1, error => error.ToString())).IsError("5");
+        await Assert.That(Result.Error<int, string>("5").Map(i => i + 1, error => new Exception(error))).IsError();
+        await Assert.That(Result.Error<int, string>("nay").Map(i => Result.Success<int, string>(i))).IsError("nay");
+        await Assert.That(Result.Success<int, string>(1).Map(i => Result.Error<int, string>("nay"))).IsError("nay");
+        await Assert.That(RefOption.Error<Span<char>>().Map(s => new string(s))).IsError();
+        await Assert.That(OptionsMarshall.IsSuccess(RefOption.Error<Span<char>>().Map(s => s))).IsFalse();
+        await Assert.That(OptionsMarshall.IsSuccess(Option.Error<string>().MapAsSpan())).IsFalse();
+
+
+        await Assert.That(Option.Error().Map(() => "yay", () => "nay")).IsEqualTo("nay");
+        await Assert.That(ErrorState.Error().Map(() => "yay", e => "nay")).IsEqualTo("nay");
+        await Assert.That(ErrorState.Error(1).Map(() => "yay", e => "nay")).IsEqualTo("nay");
+    }
+}
