b2c040389104c03398a2f93bbc6c2945b61c019f
Barion <ametrin.studios@gmail.com>
2025-02-10 14:07:52 +0100

    follow up on #11 for tuples

--- src/Extensions/OptionTupleExtensions.cs
+++ src/Extensions/OptionTupleExtensions.cs
@@ -8,21 +8,39 @@ public static class OptionTupleExtensions
     [Obsolete("use Map")]
     public static Option<R> Select<R, T1, T2>(this (Option<T1>, Option<T2>) options, Func<T1, T2, Option<R>> selector)
         => options.Item1._hasValue && options.Item2._hasValue ? selector(options.Item1._value, options.Item2._value) : default;
-        
+
     public static Option<R> Map<R, T1, T2>(this (Option<T1>, Option<T2>) options, Func<T1, T2, R> selector)
         => options.Item1._hasValue && options.Item2._hasValue ? selector(options.Item1._value, options.Item2._value) : default(Option<R>);
     public static Option<R> Map<R, T1, T2>(this (Option<T1>, Option<T2>) options, Func<T1, T2, Option<R>> selector)
         => options.Item1._hasValue && options.Item2._hasValue ? selector(options.Item1._value, options.Item2._value) : default;
 
-    public static void Consume<T1, T2>(this (Option<T1>, Option<T2>) options, Action<T1, T2>? success = null, Action? error = null)
+    public static R? Map<R, T1, T2>(this (T1?, T2?) options, Func<T1, T2, R> selector)
+        where T1 : class
+        where T2 : class
+        where R : class
+        => options.Item1 is not null && options.Item2 is not null ? selector(options.Item1, options.Item2) : null;
+    public static R? Map<R, T1, T2>(this (T1?, T2?) options, Func<T1, T2, R> selector)
+        where T1 : struct
+        where T2 : class
+        where R : class
+        => options.Item1.HasValue && options.Item2 is not null ? selector(options.Item1.Value, options.Item2) : null;
+    public static R? Map<R, T1, T2>(this (T1?, T2?) options, Func<T1, T2, R> selector)
+        where T1 : class
+        where T2 : struct
+        where R : class
+        => options.Item1 is not null && options.Item2.HasValue ? selector(options.Item1, options.Item2.Value) : null;
+
+    public static Option Consume<T1, T2>(this (Option<T1>, Option<T2>) options, Action<T1, T2>? success = null, Action? error = null)
     {
         if (options.Item1._hasValue && options.Item2._hasValue)
         {
             success?.Invoke(options.Item1._value, options.Item2._value);
+            return true;
         }
         else
         {
             error?.Invoke();
+            return false;
         }
     }
 
@@ -37,4 +55,84 @@ public static class OptionTupleExtensions
             error?.Invoke();
         }
     }
+
+    public static bool Consume<T1, T2>(this (T1?, T2?) options, Action<T1, T2>? success = null, Action? error = null)
+        where T1 : class
+        where T2 : class
+    {
+        if (options.Item1 is not null && options.Item2 is not null)
+        {
+            success?.Invoke(options.Item1, options.Item2);
+            return true;
+        }
+        else
+        {
+            error?.Invoke();
+            return false;
+        }
+    }
+    public static bool Consume<T1, T2>(this (T1?, T2?) options, Action<T1, T2>? success = null, Action? error = null)
+        where T1 : struct
+        where T2 : class
+    {
+        if (options.Item1.HasValue && options.Item2 is not null)
+        {
+            success?.Invoke(options.Item1.Value, options.Item2);
+            return true;
+        }
+        else
+        {
+            error?.Invoke();
+            return false;
+        }
+    }
+    public static bool Consume<T1, T2>(this (T1?, T2?) options, Action<T1, T2>? success = null, Action? error = null)
+        where T1 : class
+        where T2 : struct
+    {
+        if (options.Item1 is not null && options.Item2.HasValue)
+        {
+            success?.Invoke(options.Item1, options.Item2.Value);
+            return true;
+        }
+        else
+        {
+            error?.Invoke();
+            return false;
+        }
+    }
+    public static bool Consume<T1, T2>(this (T1?, T2?) options, Action<T1, T2>? success = null, Action? error = null)
+        where T1 : struct
+        where T2 : struct
+    {
+        if (options.Item1.HasValue && options.Item2.HasValue)
+        {
+            success?.Invoke(options.Item1.Value, options.Item2.Value);
+            return true;
+        }
+        else
+        {
+            error?.Invoke();
+            return false;
+        }
+    }
+}
+
+public static class OptionValueTupleExtensions
+{
+    public static R? Map<R, T1, T2>(this (T1?, T2?) options, Func<T1, T2, R> selector)
+        where T1 : class
+        where T2 : class
+        where R : struct
+        => options.Item1 is not null && options.Item2 is not null ? selector(options.Item1, options.Item2) : null;
+    public static R? Map<R, T1, T2>(this (T1?, T2?) options, Func<T1, T2, R> selector)
+        where T1 : struct
+        where T2 : class
+        where R : struct
+        => options.Item1.HasValue && options.Item2 is not null ? selector(options.Item1.Value, options.Item2) : null;
+    public static R? Map<R, T1, T2>(this (T1?, T2?) options, Func<T1, T2, R> selector)
+        where T1 : class
+        where T2 : struct
+        where R : struct
+        => options.Item1 is not null && options.Item2.HasValue ? selector(options.Item1, options.Item2.Value) : null;
 }
