1d980e1115bc6293c86ecc99b8253ccdbe069c9f
Barion <ametrin.studios@gmail.com>
2025-02-10 21:28:35 +0100

    MapAsync follow up

--- src/Operations/MapAsync.cs
+++ src/Operations/MapAsync.cs
@@ -17,6 +17,8 @@ partial struct Result<TValue>
         => _hasValue ? await map(_value) : _error;
     public Task<Result<TResult>> MapAsync<TResult>(Func<TValue, Task<Result<TResult>>> map)
         => _hasValue ? map(_value) : Task.FromResult(Result.Error<TResult>(_error));
+    public async Task<Result<TResult, TError>> MapAsync<TResult, TError>(Func<TValue, Task<TResult>> map, Func<Exception, TError> errorMap)
+        => _hasValue ? await map(_value) : errorMap(_error);
 }
 
 partial struct Result<TValue, TError>
@@ -25,21 +27,54 @@ partial struct Result<TValue, TError>
         => _hasValue ? await map(_value) : _error;
     public Task<Result<TResult, TError>> MapAsync<TResult>(Func<TValue, Task<Result<TResult, TError>>> map)
         => _hasValue ? map(_value) : Task.FromResult(Result.Error<TResult, TError>(_error));
+    [OverloadResolutionPriority(1)]
+    public async Task<Result<TResult>> MapAsync<TResult>(Func<TValue, Task<TResult>> map, Func<TError, Exception> errorMap)
+        => _hasValue ? await map(_value) : errorMap(_error);
+    public async Task<Result<TResult, TNewError>> MapAsync<TResult, TNewError>(Func<TValue, Task<TResult>> map, Func<TError, TNewError> errorMap)
+        => _hasValue ? await map(_value) : errorMap(_error);
 }
 
 public static class OptionMapAsyncExtensions
 {
-    public static async Task<Option<TResult>> Map<TValue, TResult>(this Task<Option<TValue>> optionTask, Func<TValue, TResult> map)
+    public static async Task<Option<TResult>> MapAsync<TValue, TResult>(this Task<Option<TValue>> optionTask, Func<TValue, TResult> map)
         => (await optionTask).Map(map); // ContinueWith is 42x slower and uses 2.7x memory (.NET 9)
+    [OverloadResolutionPriority(1)]
+    public static async Task<Option<TResult>> MapAsync<TValue, TResult>(this Task<Option<TValue>> optionTask, Func<TValue, Option<TResult>> map)
+        => (await optionTask).Map(map);
     public static async Task<Option<TResult>> MapAsync<TValue, TResult>(this Task<Option<TValue>> optionTask, Func<TValue, Task<TResult>> map)
         => await (await optionTask).MapAsync(map);
     public static async Task<Option<TResult>> MapAsync<TValue, TResult>(this Task<Option<TValue>> optionTask, Func<TValue, Task<Option<TResult>>> map)
         => await (await optionTask).MapAsync(map);
 
-    public static async Task<Result<TResult>> Map<TValue, TResult>(this Task<Result<TValue>> optionTask, Func<TValue, TResult> map)
+    public static async Task<Result<TResult>> MapAsync<TValue, TResult>(this Task<Result<TValue>> optionTask, Func<TValue, TResult> map)
         => (await optionTask).Map(map);
+    public static async Task<Result<TResult>> MapAsync<TValue, TResult>(this Task<Result<TValue>> optionTask, Func<TValue, Result<TResult>> map)
+        => (await optionTask).Map(map);
+    public static async Task<Result<TResult, TNewError>> MapAsync<TValue, TResult, TNewError>(this Task<Result<TValue>> optionTask, Func<TValue, TResult> map, Func<Exception, TNewError> errorMap)
+        => (await optionTask).Map(map, errorMap);
     public static async Task<Result<TResult>> MapAsync<TValue, TResult>(this Task<Result<TValue>> resultTask, Func<TValue, Task<TResult>> map)
         => await (await resultTask).MapAsync(map);
     public static async Task<Result<TResult>> MapAsync<TValue, TResult>(this Task<Result<TValue>> resultTask, Func<TValue, Task<Result<TResult>>> map)
         => await (await resultTask).MapAsync(map);
+    public static async Task<Result<TResult, TError>> MapAsync<TValue, TResult, TError>(this Task<Result<TValue>> resultTask, Func<TValue, Task<TResult>> map, Func<Exception, TError> errorMap)
+        => await (await resultTask).MapAsync(map, errorMap);
+
+    public static async Task<Result<TResult, TError>> MapAsync<TValue, TError, TResult>(this Task<Result<TValue, TError>> optionTask, Func<TValue, TResult> map)
+        => (await optionTask).Map(map);
+    
+    [OverloadResolutionPriority(1)]
+    public static async Task<Result<TResult, Exception>> MapAsync<TValue, TError, TResult>(this Task<Result<TValue, TError>> optionTask, Func<TValue, TResult> map, Func<TError, Exception> errorMap)
+        => (await optionTask).Map(map, errorMap);
+    public static async Task<Result<TResult, TNewError>> MapAsync<TValue, TError, TResult, TNewError>(this Task<Result<TValue, TError>> optionTask, Func<TValue, TResult> map, Func<TError, TNewError> errorMap)
+        => (await optionTask).Map(map, errorMap);
+    public static async Task<Result<TResult, TError>> MapAsync<TValue, TError, TResult>(this Task<Result<TValue, TError>> resultTask, Func<TValue, Task<TResult>> map)
+        => await (await resultTask).MapAsync(map);
+    public static async Task<Result<TResult, TError>> MapAsync<TValue, TError, TResult>(this Task<Result<TValue, TError>> resultTask, Func<TValue, Task<Result<TResult, TError>>> map)
+        => await (await resultTask).MapAsync(map);
+    
+    [OverloadResolutionPriority(1)]
+    public static async Task<Result<TResult>> MapAsync<TValue, TError, TResult>(this Task<Result<TValue, TError>> resultTask, Func<TValue, Task<TResult>> map, Func<TError, Exception> errorMap)
+        => await (await resultTask).MapAsync(map, errorMap);
+    public static async Task<Result<TResult, TNewError>> MapAsync<TValue, TError, TResult, TNewError>(this Task<Result<TValue, TError>> resultTask, Func<TValue, Task<TResult>> map, Func<TError, TNewError> errorMap)
+        => await (await resultTask).MapAsync(map, errorMap);
 }
---
+++ test/MapAsyncTests.cs
@@ -0,0 +1,64 @@
+namespace Ametrin.Optional.Test;
+
+public sealed class MapAsyncTests
+{
+#pragma warning disable CS1998 // lacks await operators
+    [Test]
+    public async Task MapAsync_Success_Test()
+    {
+        await Assert.That(await Option.Success(1).MapAsync(async i => i + 1)).IsSuccess(2);
+        await Assert.That(await Option.Success(1).MapAsync(async i => Option.Success(i))).IsSuccess(1);
+        await Assert.That(await Result.Success(1).MapAsync(async i => i + 1)).IsSuccess(2);
+        await Assert.That(await Result.Success(1).MapAsync(async i => i + 1, e => e.Message)).IsSuccess(2);
+        await Assert.That(await Result.Success(1).MapAsync(async i => Result.Success(i))).IsSuccess(1);
+        await Assert.That(await Result.Success<int, string>(1).MapAsync(async i => i + 1)).IsSuccess(2);
+        await Assert.That(await Result.Success<int, int>(1).MapAsync(async i => i + 1, error => error.ToString())).IsSuccess(2);
+        await Assert.That(await Result.Success<int, string>(5).MapAsync(async i => i + 1, error => new FormatException(error))).IsSuccess();
+        await Assert.That(await Result.Success<int, string>(1).MapAsync(async i => Result.Success<int, string>(i))).IsSuccess(1);
+    }
+
+    [Test]
+    public async Task Task_MapAsync_Success_Test()
+    {
+        var optionTask = Task.FromResult(Option.Success(1));
+        var resultTask = Task.FromResult(Result.Success(1));
+        var result2Task = Task.FromResult(Result.Success<int, string>(1));
+        await Assert.That(await optionTask.MapAsync(async i => i + 1)).IsSuccess(2);
+        await Assert.That(await optionTask.MapAsync(async i => Option.Success(i))).IsSuccess(1);
+        await Assert.That(await resultTask.MapAsync(async i => i + 1)).IsSuccess(2);
+        await Assert.That(await resultTask.MapAsync(async i => i + 1, e => e.Message)).IsSuccess(2);
+        await Assert.That(await resultTask.MapAsync(async i => Result.Success(i))).IsSuccess(1);
+        await Assert.That(await result2Task.MapAsync(async i => i + 1)).IsSuccess(2);
+        await Assert.That(await Task.FromResult(Result.Success<int, int>(1)).MapAsync(async i => i + 1, error => error.ToString())).IsSuccess(2);
+        await Assert.That(await result2Task.MapAsync(async i => i + 1, error => new FormatException(error))).IsSuccess();
+        await Assert.That(await result2Task.MapAsync(async i => Result.Success<int, string>(i))).IsSuccess(1);
+
+
+        await Assert.That(await optionTask.MapAsync(i => i + 1)).IsSuccess(2);
+        await Assert.That(await optionTask.MapAsync(i => Option.Success(i))).IsSuccess(1);
+        await Assert.That(await resultTask.MapAsync(i => i + 1)).IsSuccess(2);
+        await Assert.That(await resultTask.MapAsync(i => i + 1, e => e.Message)).IsSuccess(2);
+        await Assert.That(await resultTask.MapAsync(i => Result.Success(i))).IsSuccess(1);
+        await Assert.That(await result2Task.MapAsync(i => i + 1)).IsSuccess(2);
+        await Assert.That(await Task.FromResult(Result.Success<int, int>(1)).MapAsync(i => i + 1, error => error.ToString())).IsSuccess(2);
+        await Assert.That(await result2Task.MapAsync(i => i + 1, error => new FormatException(error))).IsSuccess();
+        await Assert.That(await result2Task.MapAsync(i => Result.Success<int, string>(i))).IsSuccess(1);
+    }
+
+    [Test]
+    public async Task MapAsnyc_Error_Test()
+    {
+        await Assert.That(Option.Error<int>().MapAsync(async i => i + 1)).IsError();
+        await Assert.That(Option.Error<int>().MapAsync(async i => Option.Success(i))).IsError();
+        await Assert.That(Option.Success(1).MapAsync(async i => Option.Error<int>())).IsError();
+        await Assert.That(Result.Error<int>().MapAsync(async i => i + 1)).IsError();
+        await Assert.That(Result.Error<int>().MapAsync(async i => i + 1, e => e.Message)).IsError();
+        await Assert.That(Result.Error<int>(new FormatException()).MapAsync(async i => Result.Success(i))).IsErrorOfType<int, FormatException>();
+        await Assert.That(Result.Success(1).MapAsync(async i => Result.Error<int>(new ArgumentException()))).IsErrorOfType<int, ArgumentException>();
+        await Assert.That(Result.Error<int, string>("").MapAsync(async i => i + 1)).IsError("");
+        await Assert.That(Result.Error<int, int>(5).MapAsync(async i => i + 1, error => error.ToString())).IsError("5");
+        await Assert.That(Result.Error<int, string>("5").MapAsync(async i => i + 1, error => new FormatException(error))).IsError();
+        await Assert.That(Result.Error<int, string>("nay").MapAsync(async i => Result.Success<int, string>(i))).IsError("nay");
+        await Assert.That(Result.Success<int, string>(1).MapAsync(async i => Result.Error<int, string>("nay"))).IsError("nay");
+    }
+}
