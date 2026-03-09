de7f80f07616c435da4c0087684d9f75502ba22f
Barion <barion+git@ik.me>
2026-03-09 08:40:56 +0100

    tweaks

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
+# dotnet clean
 Remove-Item "$env:USERPROFILE\.nuget\packages\ametrin.optional.analyzer" -Recurse
 Remove-Item "$env:USERPROFILE\.nuget\packages\ametrin.optional" -Recurse
 # Remove-Item "$env:USERPROFILE\.nuget\packages\ametrin.optional.testing.tunit" -Recurse
-# dotnet clean
 dotnet pack .\analyzer\ -c release
 dotnet pack .\src\ -c release
 dotnet pack .\testing\TUnit\ -c release
