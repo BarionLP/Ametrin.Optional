42587a1a6101df8aa51e5a8616c10175bd6d66af
Barion <barion+git@ik.me>
2026-08-11 01:26:56 +0200

    tweaks

--- src/UnionBackend.cs
+++ src/UnionBackend.cs
@@ -1,17 +1,16 @@
+#if NET11_0_OR_GREATER
 using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
 
 namespace Ametrin.Optional;
 
-#if NET11_0_OR_GREATER
 [Union]
 partial struct Option<TValue> : Option<TValue>.IUnionMembers
 {
-    [EditorBrowsable(EditorBrowsableState.Never)]
+    [Obsolete("do not use")]
     object? IUnionMembers.Value => _value;
 
-    [EditorBrowsable(EditorBrowsableState.Never)]
-    public bool TryGetValue([MaybeNullWhen(false)] out TValue value)
+    bool IUnionMembers.TryGetValue([MaybeNullWhen(false)] out TValue value)
     {
         value = _value;
         return _hasValue;
@@ -20,7 +19,9 @@ partial struct Option<TValue> : Option<TValue>.IUnionMembers
     public interface IUnionMembers
     {
         static Option<TValue> Create(TValue value) => Option.Success(value);
+        [Obsolete("do not use")]
         object? Value { get; }
+        bool TryGetValue([MaybeNullWhen(false)] out TValue value);
     }
 }
 #endif
